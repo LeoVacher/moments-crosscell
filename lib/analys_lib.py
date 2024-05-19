f3b3513a97969dc020125ab15e4f56cf24bf9d16
LeoVacher <57897650+LeoVacher@users.noreply.github.com>
2024-05-19 15:50:44 +0200

    update

--- lib/analys_lib.py
+++ lib/analys_lib.py
@@ -278,32 +278,32 @@ def fito1_bT_moms_nocterm(nucross,DL,Linv,resultsmbb_PL,quiet=True,fix=1):
     results={'A' : paramiterl[:,:,0], 'beta' : paramiterl[:,:,1], 'temp' : paramiterl[:,:,2], 'A_s':paramiterl[:,:,3] , 'beta_s':paramiterl[:,:,4], 'A_sd':paramiterl[:,:,5], 'Aw1b' : paramiterl[:,:,6], 'w1bw1b' : paramiterl[:,:,7],'Aw1t' : paramiterl[:,:,8],'w1bw1t' : paramiterl[:,:,9],'w1tw1t' : paramiterl[:,:,10],'Asw1bs' : paramiterl[:,:,11],'w1bsw1bs' : paramiterl[:,:,12],'r' : paramiterl[:,:,13], 'X2red': chi2l}
     return results
 
-def fito1_bT_moms_full(nucross,DL,Linv,resultsmbb_PL,quiet=True,fix=1):
-    """
-    Fit using a first order moment expansion in both beta and T on a DL
-    :param: nucross, array of the cross-frequencies
-    :param DL: The input binned DL array should be of the shape (Nsim, Ncross, Nell)
-    :param Linv: inverse of the Cholesky matrix
-    :param quiet: display output of the fit for debugging
-    :param resultsmbb: must be input mbb best fit in the format of fitmbb()
-    :return results: dictionnary containing A, beta, temp, Aw1b, w1bw1b, r and X2red for each (ell,n)
-    """
-    N,_,Nell=DL.shape
-    nparam=14
-    paramiterl=np.zeros((Nell,N,nparam+1))
-    chi2l=np.zeros((Nell,N))
-    funcfit=mpl.FitdscbetaTbetas_full
-    for L in range(0,Nell):
-        print("%s%%"%(L*100/Nell))
-        for n in range(N):
-            # first o1 fit, dust fixed, mom free, r fixed
-            parinfopl = [{'value':resultsmbb_PL['A'][L,n], 'fixed':fix},{'value':resultsmbb_PL['beta'][L,n], 'fixed':fix},{'value':resultsmbb_PL['temp'][L,n], 'fixed':fix},{'value':resultsmbb_PL['A_s'][L,n], 'fixed':fix},{'value':resultsmbb_PL['beta_s'][L,n], 'fixed':fix},{'value':resultsmbb_PL['A_sd'][L,n], 'fixed':fix},{'value':0, 'fixed':0},{'value':0, 'fixed':0},{'value':0, 'fixed':0},{'value':0, 'fixed':0},{'value':0, 'fixed':0}, {'value':0, 'fixed':0},{'value':0, 'fixed':0},{'value':0, 'fixed':0},{'value':L, 'fixed':1}] #dust params
-            fa = {'x':nucross, 'y':DL[n,:,L], 'err': Linv[L]}
-            m = mpfit(funcfit,parinfo= parinfopl ,functkw=fa,quiet=quiet)
-            paramiterl[L,n]= m.params
-            chi2l[L,n]=m.fnorm/m.dof            
-    results={'A' : paramiterl[:,:,0], 'beta' : paramiterl[:,:,1], 'temp' : paramiterl[:,:,2], 'A_s':paramiterl[:,:,3] , 'beta_s':paramiterl[:,:,4], 'A_sd':paramiterl[:,:,5], 'Aw1b' : paramiterl[:,:,6], 'w1bw1b' : paramiterl[:,:,7],'Aw1t' : paramiterl[:,:,8],'w1bw1t' : paramiterl[:,:,9],'w1tw1t' : paramiterl[:,:,10],'Asw1bs' : paramiterl[:,:,11],'w1bsw1bs' : paramiterl[:,:,12],'r' : paramiterl[:,:,13], 'X2red': chi2l}
-    return results
+# def fito1_bT_moms_full(nucross,DL,Linv,resultsmbb_PL,quiet=True,fix=1):
+#     """
+#     Fit using a first order moment expansion in both beta and T on a DL
+#     :param: nucross, array of the cross-frequencies
+#     :param DL: The input binned DL array should be of the shape (Nsim, Ncross, Nell)
+#     :param Linv: inverse of the Cholesky matrix
+#     :param quiet: display output of the fit for debugging
+#     :param resultsmbb: must be input mbb best fit in the format of fitmbb()
+#     :return results: dictionnary containing A, beta, temp, Aw1b, w1bw1b, r and X2red for each (ell,n)
+#     """
+#     N,_,Nell=DL.shape
+#     nparam=14
+#     paramiterl=np.zeros((Nell,N,nparam+1))
+#     chi2l=np.zeros((Nell,N))
+#     funcfit=mpl.FitdscbetaTbetas_full
+#     for L in range(0,Nell):
+#         print("%s%%"%(L*100/Nell))
+#         for n in range(N):
+#             # first o1 fit, dust fixed, mom free, r fixed
+#             parinfopl = [{'value':resultsmbb_PL['A'][L,n], 'fixed':fix},{'value':resultsmbb_PL['beta'][L,n], 'fixed':fix},{'value':resultsmbb_PL['temp'][L,n], 'fixed':fix},{'value':resultsmbb_PL['A_s'][L,n], 'fixed':fix},{'value':resultsmbb_PL['beta_s'][L,n], 'fixed':fix},{'value':resultsmbb_PL['A_sd'][L,n], 'fixed':fix},{'value':0, 'fixed':0},{'value':0, 'fixed':0},{'value':0, 'fixed':0},{'value':0, 'fixed':0},{'value':0, 'fixed':0}, {'value':0, 'fixed':0},{'value':0, 'fixed':0},{'value':0, 'fixed':0},{'value':L, 'fixed':1}] #dust params
+#             fa = {'x':nucross, 'y':DL[n,:,L], 'err': Linv[L]}
+#             m = mpfit(funcfit,parinfo= parinfopl ,functkw=fa,quiet=quiet)
+#             paramiterl[L,n]= m.params
+#             chi2l[L,n]=m.fnorm/m.dof            
+#     results={'A' : paramiterl[:,:,0], 'beta' : paramiterl[:,:,1], 'temp' : paramiterl[:,:,2], 'A_s':paramiterl[:,:,3] , 'beta_s':paramiterl[:,:,4], 'A_sd':paramiterl[:,:,5], 'Aw1b' : paramiterl[:,:,6], 'w1bw1b' : paramiterl[:,:,7],'Aw1t' : paramiterl[:,:,8],'w1bw1t' : paramiterl[:,:,9],'w1tw1t' : paramiterl[:,:,10],'Asw1bs' : paramiterl[:,:,11],'w1bsw1bs' : paramiterl[:,:,12],'r' : paramiterl[:,:,13], 'X2red': chi2l}
+#     return results
 
 #PLOT FUNCTIONS ##################################################################################################################
 
