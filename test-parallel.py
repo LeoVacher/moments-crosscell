a785ef8b1d3ae71047a3e6b52f158465ffa2656e
LeoVacher <57897650+LeoVacher@users.noreply.github.com>
2024-05-20 15:55:44 +0200

    Update test-parallel.py

--- test-parallel.py
+++ test-parallel.py
@@ -44,7 +44,7 @@ Nell = len(l)
 
 #instrument informations:
 
-instr_name='LiteBIRD_full'
+instr_name='Litebird_full'
 instr =  np.load("./lib/instr_dict/%s.npy"%instr_name,allow_pickle=True).item()
 freq = instr['frequencies']
 freq=freq
