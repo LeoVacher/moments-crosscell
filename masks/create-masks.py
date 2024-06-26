3666a423059279cf8d243454717dc1ea71b6abc3
LeoVacher <57897650+LeoVacher@users.noreply.github.com>
2024-06-26 14:52:49 +0900

    update masks

--- masks/create-masks.py
+++ masks/create-masks.py
@@ -18,16 +18,19 @@ import analys_lib as an
 import simu_lib as sim
 import pysm3.units as u
 
-fsky=0.8
+fsky=0.9
 scale=10
 nside=64
 
 fskylist=np.array([0.2,0.4,0.6,0.7,0.8,0.9,0.97,0.99])
 field= list(np.where(fskylist==fsky)[0])
-mask0 = hp.read_map("./masks/HFI_Mask_GalPlane-apo0_2048_R2.00.fits", field=field)
-mask0 = nmt.mask_apodization(mask0, scale, apotype="C2")
-mask = hp.pixelfunc.ud_grade(mask0, nside, dtype=float)
 
-#hp.mollview(mask)
+maskgal =hp.read_map("./masks/HFI_Mask_GalPlane-apo0_2048_R2.00.fits", field=field)
+maskgal = hp.ud_grade(maskgal*1.,nside_out=nside)
+maskgal[maskgal<0.5] = 0
+maskgal[maskgal>=0.5] = 1
+maskgalapo = nmt.mask_apodization(maskgal*1., scale, apotype='C2')
 
-hp.write_map("./masks/mask_fsky%s_nside%s_aposcale%s.npy"%(fsky,nside,scale), mask, dtype=float, overwrite=True)
+hp.mollview(maskgalapo)
+
+hp.write_map("./masks/mask_fsky%s_nside%s_aposcale%s.npy"%(fsky,nside,scale), maskgalapo, dtype=float, overwrite=True)
