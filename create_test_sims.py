1357dc2adcb8c9f74d770985b621a88fd21c644f
LeoVacher <57897650+LeoVacher@users.noreply.github.com>
2024-06-27 22:16:51 +0900

    Update create_test_sims.py

--- create_test_sims.py
+++ create_test_sims.py
@@ -21,14 +21,14 @@ import pysm3.units as u
 r = 0
 nside = 64
 Npix = hp.nside2npix(nside)
-N=500 
+N=10000 
 lmax = nside*3-1
 #lmax=850
 scale = 10
 Nlbin = 10
 fsky = 0.7
-dusttype = None
-syncrotype = None
+dusttype = 0
+syncrotype = 0
 kw = ''
 
 # instr param
