25545fb3121ba903d24f7abd0cd38b470d4997c0
LeoVacher <57897650+LeoVacher@users.noreply.github.com>
2024-10-25 12:51:54 +0200

    update

--- emcee-d0s0.py
+++ emcee-d0s0.py
@@ -1,14 +1,147 @@
+import sys
+sys.path.append("./lib")
 import numpy as np
-def log_prob(x, mu, cov):
-    diff = x - mu
-    return -0.5 * np.dot(diff, np.linalg.solve(cov, diff))
+import pymaster as nmt 
+import pysm3
+import time
+from mpfit import mpfit
+import mpfitlib as mpl
+import scipy
+#from Nearest_Positive_Definite import *
+from matplotlib.backends.backend_pdf import PdfPages
+import matplotlib.patheffects as path_effects
+import scipy.stats as st
+import basicfunc as func
+import analys_lib as an
+from plotlib import plotr_gaussproduct
 
-ndim = 5
+r=0.
+nside = 64
+lmax = nside*3-1
+#lmax=850
+scale = 10
+Nlbin = 10
+fsky = 0.8
+ELLBOUND = 15
+dusttype = 0
+synctype = 0
+kw=''
+kwsim=''
+Pathload='./'
 
-np.random.seed(42)
-means = np.random.rand(ndim)
+# Call C_ell of simulation
 
-cov = 0.5 - np.random.rand(ndim**2).reshape((ndim, ndim))
-cov = np.triu(cov)
-cov += cov.T - np.diag(cov.diagonal())
-cov = np.dot(cov, cov)
+if synctype==None:
+    DLdc = np.load(Pathload+"/CLsimus/DLcross_nside%s_fsky%s_scale%s_Nlbin%s_d%sc.npy"%(nside,fsky,scale,Nlbin,dusttype))
+else:
+    DLdc = np.load(Pathload+"/CLsimus/DLcross_nside%s_fsky%s_scale%s_Nlbin%s_d%ss%sc.npy"%(nside,fsky,scale,Nlbin,dusttype,synctype))
+
+# Initialize binning scheme with Nlbin ells per bandpower
+
+b = nmt.bins.NmtBin(nside=nside,lmax=lmax,nlb=Nlbin)
+l = b.get_effective_ells()
+l = l[:ELLBOUND]
+Nell = len(l)
+
+#instrument informations:
+
+instr_name='litebird_full'
+instr =  np.load("./lib/instr_dict/%s.npy"%instr_name,allow_pickle=True).item()
+freq = instr['frequencies']
+freq=freq
+nf = len(freq)
+Ncross = int(nf*(nf+1)/2)
+kw="_bin30"
+
+#compute cross-frequencies 
+
+nucross = []
+for i in range(0,nf):
+    for j in range(i,nf):
+        nucross.append(np.sqrt(freq[i]*freq[j]))
+nucross = np.array(nucross)
+
+#N = len(DLdc[:,0,0]) #in order to have a quicker run, replace by e.g. 50 or 100 here for testing.
+N=500
+
+DLdc=DLdc[:N,:,:Nell]
+
+#compute Cholesky matrix:
+
+data= np.array([DLdc[0]])
+stat= DLdc[1:]
+Linvdc=an.getLinvdiag(stat,printdiag=True)
+
+# fit MBB, get results and save
+
+p0=[100, 1.54, 20, 10, -3,1, 0] #first guess for mbb A, beta, T, r
+
+results_ds_o0 = an.fit_mom('ds_o0',nucross,data,Linvdc,p0,quiet=True)
+
+
+DLdctempo = np.swapaxes(DLdc,0,1)
+for L in range(Nell):
+    covariancedc.append(np.cov(DLdctempo[:,:,2,L]))
+    invcovdc.append(scipy.linalg.inv(np.array(covariancedc[L])))
+
+def lnlike(theta, x, y, yerr,yerrminusone,l0):
+    model = eml.modeldcordre0(theta,x,l0)
+    chi2 = np.dot(np.dot(y-model,yerrminusone),y-model)
+    return -0.5*(np.sum(chi2))
+
+# def lnprior(theta):
+    # Amp, beta, r = theta
+    # if -np.inf < Amp < np.inf and -np.inf < beta < np.inf and -np.inf < r < np.inf:
+    #     return 0.0
+    # return -np.inf
+
+def lnprob(theta, x, y, yerr,yerrminusone,l0):
+    # lp = lnprior(theta)
+    # if not np.isfinite(lp):
+    #     return -np.inf
+    return  lnlike(theta, x, y, yerr,yerrminusone,l0) #+lp
+
+n = 0
+L = 0
+
+mcmcparamiterl = []#list(np.load("/home/lvacher/emceefit/mcmcparamiterld0c.npy"))
+chi2l = []# list(np.load("/home/lvacher/emceefit/mcmcchi2ld0c.npy"))
+
+ini = len(mcmcparamiterl)
+
+for L in range(ini,Nell):
+    mcmcparamitern = []
+    chi2n=[]
+    for n in range(N):
+        time1 = time.time()
+        y = DLdc[n,:,2,L]
+        #yerr = DLstddc[:,2,L]
+        yerr =  covariancedc[L]
+        yerrminusone = invcovdc[L]
+        pl0=[AmpTrue,betaTrue,rTrue,L,temp_dust]
+        parinfopl = [{'value':pl0[0], 'fixed':0},{'value':pl0[1],'fixed':0},{'value':pl0[2], 'fixed':0},{'value':pl0[3], 'fixed':1},{'value':pl0[4], 'fixed':1}]
+        fa = {'x':nucross, 'y':DLdc[n,:,2,L], 'err': Linvdc[L]}
+        m = mpfit(mpl.Fitdcordre0,parinfo= parinfopl ,functkw=fa)
+        thetafit = [m.params[0], m.params[1],m.params[2]]
+        ndim, nwalkers,chainlength,burnt = 3, 6, 600,200
+        pos = [np.array(thetafit) + 1e-6*np.random.randn(ndim) for i in range(nwalkers)]
+        sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, args=(x, y, yerr,yerrminusone,L))
+        sampler.run_mcmc(pos, chainlength)
+        samples = sampler.get_chain()
+        samples = sampler.chain[:, burnt:, :].reshape((-1, ndim))
+
+        amp_mcmc, beta_mcmc, r_mcmc = map(lambda v: (v[1], v[2]-v[1], v[1]-v[0]),zip(*np.percentile(samples, [16, 50, 84],axis=0)))
+        theta_mcmc = np.array([amp_mcmc[0],beta_mcmc[0],r_mcmc[0]])
+        mcmcparamitern.append(np.array([amp_mcmc,beta_mcmc,r_mcmc]))
+        print('n=',n)
+        print('L=',L)
+        model = eml.modeldcordre0(theta_mcmc,x,L)
+        chi2tempo = np.dot(np.dot(y-model,yerrminusone),y-model)
+        chi2n.append(chi2tempo)
+
+        time2 = time.time()
+        print(time2-time1)
+    mcmcparamiterl.append(np.array(mcmcparamitern))
+    chi2l.append(np.array(chi2n))
+    np.save("/home/lvacher/emceefit/mcmcparamiterld0c1o0",mcmcparamiterl)
+    np.save("/home/lvacher/emceefit/mcmcchi2ld0c1o0",chi2l)
---
+++ lib/emcee-lib.py
@@ -0,0 +1,99 @@
+import numpy as np
+import basicfunc as func
+import pysm3
+import healpy as hp
+import pymaster as nmt
+import numpy as np
+import pysm_common as psm 
+import basicfunc as func
+
+#contains all the functions to be fitted by mpfit 
+
+nside = 64
+lmax = nside*3-1
+Nlbin = 10 
+ELLBOUND = 19
+
+b = nmt.bins.NmtBin(nside=nside,lmax=lmax,nlb=Nlbin)
+l = b.get_effective_ells()
+
+CLcmb_or=hp.read_cl('./CLsimus/Cls_Planck2018_r0.fits') #TT EE BB TE
+CL_tens=hp.read_cl('./CLsimus/Cls_Planck2018_tensor_r1.fits')
+
+DL_lensbin = l*(l+1)*b.bin_cell(CLcmb_or[2,2:lmax+3])[0:ELLBOUND]/2/np.pi
+DL_tens = l*(l+1)*b.bin_cell(CL_tens[2,2:lmax+3])[0:ELLBOUND]/2/np.pi
+ 
+def Gaussian(p,fjac=None, x=None, y=None, err=None):
+    # Gaussian curve
+    model = func.Gaussian(x,p[0],p[1])
+    status = 0
+    return([status, (y-model)/err])
+
+def func_d_o0(p,fjac=None, x1=None, x2=None, y=None, err=None,nuref=353.):
+    #fit function dust, order 0
+
+    nu_i=x1
+    nu_j=x2
+    mbb = p[0] * func.mbb_uK(nu_i, p[1], p[2]) * func.mbb_uK(nu_j, p[1], p[2])
+    model = mbb  + DL_lensbin[int(p[4])] + p[3] * DL_tens[int(p[4])]
+    status = 0
+    return([status, np.dot(np.transpose(y-model), err)])
+
+def func_ds_o0(p,fjac=None, x1=None, x2=None, y=None, err=None,nuref=353.,nurefs=23.,ell=None):
+    #fit function dust+syncrotron, order 0
+    nu_i=x1
+    nu_j=x2
+    mbb = p[0] * func.mbb_uK(nu_i, p[1], p[2],nu0=nuref) * func.mbb_uK(nu_j, p[1], p[2],nu0=nuref)
+    sync = p[3] * func.PL_uK(nu_i, p[4],nu0=nurefs) * func.PL_uK(nu_j, p[4],nu0=nurefs)
+    crossdustsync = p[5] * (func.mbb_uK(nu_i, p[1], p[2],nu0=nuref) * func.PL_uK(nu_j, p[4],nu0=nurefs) + func.PL_uK(nu_i, p[4],nu0=nurefs) * func.mbb_uK(nu_j, p[1], p[2],nu0=nuref))
+    model = mbb + sync + crossdustsync + DL_lensbin[ell] + p[6] * DL_tens[ell]
+    status = 0
+    return([status, np.dot(np.transpose(y-model), err)])
+
+def func_ds_o1bt(p,fjac=None, x1=None, x2=None, y=None, err=None,nuref=353,nurefs=23.,ell=None):
+    #fit function dust+syncrotron, order 1 in beta and T
+    nu_i=x1
+    nu_j=x2
+
+    ampl = func.mbb_uK(nu_i,p[1],p[2],nu0=nuref)*func.mbb_uK(nu_j,p[1],p[2],nu0=nuref)
+    sync= p[3]*func.PL_uK(nu_i,p[4],nu0=nurefs)*func.PL_uK(nu_j,p[4],nu0=nurefs)
+    crossdustsync= p[5]*(func.mbb_uK(nu_i,p[1],p[2],nu0=nuref)*func.PL_uK(nu_j,p[4],nu0=nurefs)+ func.PL_uK(nu_i,p[4],nu0=nurefs)*func.mbb_uK(nu_j,p[1],p[2],nu0=nuref))
+    lognui = np.log(nu_i/nuref)
+    lognuj = np.log(nu_j/nuref)
+    dx0 = func.dmbbT(nuref,p[2])
+    dxi = func.dmbbT(nu_i,p[2])
+    dxj = func.dmbbT(nu_j,p[2])
+    temp = ampl * (p[0]+ (lognui+lognuj) * p[6]+ lognui*lognuj * p[7])
+    temp2= ampl * ((dxi+dxj-2*dx0)*p[8]+(lognuj*(dxi-dx0)+lognui*(dxj-dx0))*p[9]+(dxi-dx0)*(dxj-dx0)*p[10])
+    crossdustsync2 = p[11]*(func.mbb_uK(nu_i,p[1],p[2],nu0=nuref)*lognui*func.PL_uK(nu_j,p[4],nu0=nurefs)+ func.PL_uK(nu_i,p[4],nu0=nurefs)*func.mbb_uK(nu_j,p[1],p[2],nu0=nuref)*lognuj)
+    crossdustsync3 = p[12]*(func.mbb_uK(nu_i,p[1],p[2],nu0=nuref)*(dxi-dx0)*func.PL_uK(nu_j,p[4],nu0=nurefs)+ func.PL_uK(nu_i,p[4],nu0=nurefs)*func.mbb_uK(nu_j,p[1],p[2],nu0=nuref)*(dxj-dx0))
+    model = temp + temp2 + sync+ crossdustsync+ crossdustsync2+ crossdustsync3+ DL_lensbin[ell] + p[13]*DL_tens[ell]
+    status = 0
+    return([status, np.dot(np.transpose(y-model), err)])
+
+def func_ds_o1bts(p,fjac=None, x1=None, x2=None, y=None, err=None,nuref=353,nurefs=23.,ell=None):
+
+    nu_i=x1
+    nu_j=x2
+
+    ampl = func.mbb_uK(nu_i,p[1],p[2])*func.mbb_uK(nu_j,p[1],p[2])
+    sync= func.PL_uK(nu_i,p[4])*func.PL_uK(nu_j,p[4])
+    crossdustsync= p[5]*(func.mbb_uK(nu_i,p[1],p[2])*func.PL_uK(nu_j,p[4])+ func.PL_uK(nu_i,p[4])*func.mbb_uK(nu_j,p[1],p[2]))/(func.PL_uK(nurefs,p[4])*func.mbb_uK(nuref,p[1],p[2]))
+    lognui = np.log(nu_i/nuref)
+    lognuj = np.log(nu_j/nuref)
+    lognuis = np.log(nu_i/nurefs)
+    lognujs = np.log(nu_j/nurefs)
+    dx0 = func.dmbbT(nuref,p[2])
+    dxi = func.dmbbT(nu_i,p[2])
+    dxj = func.dmbbT(nu_j,p[2])
+    temp = ampl * (p[0]+ (lognui+lognuj) * p[6]+ lognui*lognuj * p[7])
+    temp2 = ampl*((dxi+dxj-2*dx0)*p[8]+(lognuj*(dxi-dx0)+lognui*(dxj-dx0))*p[9]+(dxi-dx0)*(dxj-dx0)*p[10])
+    syncmom = sync * (p[3]+ (lognuis+lognujs) * p[11]+ lognuis*lognujs * p[12])
+    crossdustsync2 = p[13]*(func.mbb_uK(nu_i,p[1],p[2])*lognui*func.PL_uK(nu_j,p[4])+ func.PL_uK(nu_i,p[4])*func.mbb_uK(nu_j,p[1],p[2])*lognuj)/(func.PL_uK(nurefs,p[4])*func.mbb_uK(nuref,p[1],p[2]))
+    crossdustsync3 = p[14]*(func.mbb_uK(nu_i,p[1],p[2])*(dxi-dx0)*func.PL_uK(nu_j,p[4])+ func.PL_uK(nu_i,p[4])*func.mbb_uK(nu_j,p[1],p[2])*(dxj-dx0))/(func.PL_uK(nurefs,p[4])*func.mbb_uK(nuref,p[1],p[2]))
+    crossdustsync4 = p[15]*(func.mbb_uK(nu_i,p[1],p[2])*lognujs*func.PL_uK(nu_j,p[4])+ func.PL_uK(nu_i,p[4])*func.mbb_uK(nu_j,p[1],p[2])*lognuis)/(func.PL_uK(nurefs,p[4])*func.mbb_uK(nuref,p[1],p[2]))
+    crossdustsync5 = p[16]*(func.mbb_uK(nu_i,p[1],p[2])*lognui*func.PL_uK(nu_j,p[4])*lognujs+ func.PL_uK(nu_i,p[4])*lognuis*func.mbb_uK(nu_j,p[1],p[2])*lognuj)/(func.PL_uK(nurefs,p[4])*func.mbb_uK(nuref,p[1],p[2]))
+    crossdustsync6 = p[17]*(func.mbb_uK(nu_i,p[1],p[2])*(dxi-dx0)*func.PL_uK(nu_j,p[4])*lognujs+ func.PL_uK(nu_i,p[4])*lognuis*func.mbb_uK(nu_j,p[1],p[2])*(dxj-dx0))/(func.PL_uK(nurefs,p[4])*func.mbb_uK(nuref,p[1],p[2]))
+    model = temp + temp2 + syncmom + crossdustsync+ crossdustsync2+ crossdustsync3+crossdustsync4+crossdustsync5+crossdustsync6+ DL_lensbin[ell] + p[18]*DL_tens[ell]
+    status = 0
+    return([status, np.dot(np.transpose(y-model), err)])
