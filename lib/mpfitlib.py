ff0390d8185e48e1951ae2cb9989847ada460056
LeoVacher <57897650+LeoVacher@users.noreply.github.com>
2024-11-28 16:04:10 +0100

    Update mpfitlib.py

--- lib/mpfitlib.py
+++ lib/mpfitlib.py
@@ -101,7 +101,7 @@ def func_ds_o1bts(p,fjac=None, x1=None, x2=None, y=None, err=None,nuref=353,nure
     sync= func.PL_uK(nu_i,p[4])*func.PL_uK(nu_j,p[4])
     normcorr= np.sqrt(abs(p[0]*p[3]))
     #normcorr= 1
-    crossdustsync= p[5]*normcorr*(func.mbb_uK(nu_i,p[1],p[2])*func.PL_uK(nu_j,p[4])+ func.PL_uK(nu_i,p[4])*func.mbb_uK(nu_j,p[1],p[2]))/(func.PL_uK(nurefs,p[4])*func.mbb_uK(nuref,p[1],p[2]))
+    crossdustsync= p[5]*normcorr*(func.mbb_uK(nu_i,p[1],p[2],nu0=nuref)*func.PL_uK(nu_j,p[4],nu0=nurefs)+ func.PL_uK(nu_i,p[4],nu0=nurefs)*func.mbb_uK(nu_j,p[1],p[2],nu0=nuref))
     lognui = np.log(nu_i/nuref)
     lognuj = np.log(nu_j/nuref)
     lognuis = np.log(nu_i/nurefs)
