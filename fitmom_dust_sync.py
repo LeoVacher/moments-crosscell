8f8a11dfd325495ae4061dd37d5b8630ba0bb5a3
LeoVacher <57897650+LeoVacher@users.noreply.github.com>
2025-03-15 02:34:24 +0100

    Update fitmom_dust_sync.py

--- fitmom_dust_sync.py
+++ fitmom_dust_sync.py
@@ -22,19 +22,19 @@ Nlbin = 10
 fsky = 0.7
 dusttype = 0
 synctype = 0
-Pathload='./'
-all_ell=False #all ell or each ell independently
-fix= 0 #fix beta and T ?
-adaptative=False
-N=500
-parallel=False
-cov_type='Nmt-fg-samy' #choices: sim, Knox-fg, Knox+fg, Nmt-fg, Nmt+fg, signal.
+Pathload = './'
+all_ell = False #all ell or each ell independently
+fix = 0 #fix beta and T ?
+adaptative = False
+N = 500
+parallel = False
+cov_type = 'Nmt-fg' #choices: sim, Knox-fg, Knox+fg, Nmt-fg, Nmt+fg, signal.
 kw=''
 dusttype_cov = 0
 synctype_cov = 0
 
-if cov_type!='sim':
-    kw+='_%s'%cov_type
+if cov_type != 'sim':
+    kw += '_%s'%cov_type
 
 if parallel==True:
     comm = MPI.COMM_WORLD
@@ -55,10 +55,10 @@ Nell = len(l)
 
 #instrument informations:
 
-instr_name='litebird_full'
+instr_name = 'litebird_full'
 instr =  np.load("./lib/instr_dict/%s.npy"%instr_name,allow_pickle=True).item()
 freq = instr['frequencies']
-freq=freq
+freq = freq
 nf = len(freq)
 Ncross = int(nf*(nf+1)/2)
 
@@ -73,35 +73,35 @@ nucross = np.array(nucross)
 #compute Cholesky matrix:
 
 if np.shape(np.argwhere(DLdc == 0))[0] == 0:
-    Ncov=len(DLdc)
+    Ncov = len(DLdc)
 else:
-    Ncov=np.argwhere(DLdc == 0)[0,0]-1
+    Ncov = np.argwhere(DLdc == 0)[0,0]-1
 
-if all_ell==True:
-    if cov_type=='sim':
-        Linvdc= cvl.getLinv_all_ell(DLdc[:Ncov,:,:ELLBOUND],printdiag=True)
+if all_ell == True:
+    if cov_type == 'sim':
+        Linvdc = cvl.getLinv_all_ell(DLdc[:Ncov,:,:Nell],printdiag=True)
     else:
         cov = np.load(Pathload+"/covariances/cov_%s_nside%s_fsky%s_scale%s_Nlbin%s_d%ss%sc_all_ell.npy"%(cov_type,nside,fsky,scale,Nlbin,dusttype_cov,synctype_cov))
         Linvdc = cvl.inverse_covmat(cov, Ncross, neglect_corbins=False, return_cholesky=True, return_new=False)
 
 else:
-    if cov_type=='sim':
-        Linvdc= cvl.getLinvdiag(DLdc[:Ncov,:,:ELLBOUND],printdiag=True)
+    if cov_type == 'sim':
+        Linvdc = cvl.getLinvdiag(DLdc[:Ncov,:,:Nell],printdiag=True)
     else:
         cov = np.load(Pathload+"/covariances/cov_%s_nside%s_fsky%s_scale%s_Nlbin%s_d%ss%sc.npy"%(cov_type,nside,fsky,scale,Nlbin,dusttype_cov,synctype_cov))
         Linvdc = cvl.inverse_covmat(cov, Ncross, neglect_corbins=True, return_cholesky=True, return_new=False)
 
 #N = len(DLdc[:,0,0]) #in order to have a quicker run, replace by e.g. 50 or 100 here for testing.
-DLdc=DLdc[:N,:,:Nell]
+DLdc = DLdc[:N,:,:Nell]
 
 # fit MBB and PL, get results, save and plot
 
-p0=[100, 1.54, 20, 10, -3,0, 0] #first guess for mbb A, beta, T, A_s, beta_s, A_sd and r
+p0 = [100, 1.54, 20, 10, -3,0, 0] #first guess for mbb A, beta, T, A_s, beta_s, A_sd and r
 results_ds_o0 = an.fit_mom('ds_o0',nucross,DLdc,Linvdc,p0,quiet=True,nside=nside, Nlbin=Nlbin, fix=fix, all_ell=all_ell,kwsave='d%ss%s_%s'%(dusttype,synctype,fsky)+kw)
 
 # fit order 1 in beta and T, get results, save and plot
 
-p0=[100, 1.54, 20, 10, -3,1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0]
+p0 = [100, 1.54, 20, 10, -3,1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0]
 results_ds_o1bt = an.fit_mom('ds_o1bt',nucross,DLdc,Linvdc,p0,quiet=True,nside=nside, Nlbin=Nlbin, fix=fix,all_ell=all_ell,adaptative=adaptative,kwsave='d%ss%s_%s'%(dusttype,synctype,fsky)+kw)
 
 # fit order 1 in beta, T and beta_s, get results, save and plot
