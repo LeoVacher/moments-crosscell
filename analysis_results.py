c36478290f3afe7714a81de30d4de3c61bf0501d
LeoVacher <57897650+LeoVacher@users.noreply.github.com>
2025-03-16 10:43:56 +0100

    Update analysis_results.py

--- analysis_results.py
+++ analysis_results.py
@@ -57,5 +57,3 @@ leglist = [globals()[f"legs{i}"] for i in range(1, nres + 1)]
 collist = [globals()[f"c{i}"] for i in range(1, nres + 1)]
 
 plotrespdf(l,reslist,leglist,collist,mom_an,plot_contours=plot_contours)
-
-plotrespdf(l,[b],['d0s0_o0'],['blue'],mom_an,plot_contours=plot_contours)
