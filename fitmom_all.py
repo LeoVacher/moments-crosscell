3e3a85dc534c90683fb308b4cb94d6d29cf65923
LeoVacher <57897650+LeoVacher@users.noreply.github.com>
2025-05-11 12:27:38 +0200

    Create fitmom_all.py

---
+++ fitmom_all.py
@@ -0,0 +1,122 @@
+import sys
+sys.path.append("./lib")
+import numpy as np
+import pymaster as nmt 
+import pysm3
+import time
+from mpfit import mpfit
+import scipy
+from matplotlib.backends.backend_pdf import PdfPages
+import matplotlib.patheffects as path_effects
+import scipy.stats as st
+import basicfunc as func
+import analys_lib as an
+import covlib as cvl 
+from plotlib import plotrespdf
+
+r=0.
+nside = 64
+lmax = nside*3-1
+scale = 10
+Nlbin = 10
+fsky = 0.7
+dusttype = 1
+synctype = 1
+Pathload = './'
+adaptative = False
+N = 500
+cov_type = 'sim' #choices: sim, Knox-fg, Knox+fg, Nmt-fg, Nmt+fg, signal.
+kw=''
+dusttype_cov = dusttype
+synctype_cov = synctype
+
+if cov_type != 'sim':
+    kw += '_%s'%cov_type
+
+if parallel:
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+
+# Call C_ell of simulation
+
+if synctype == None:
+    DLdc = np.load(Pathload+"/power_spectra/DLcross_nside%s_fsky%s_scale%s_Nlbin%s_d%sc.npy"%(nside,fsky,scale,Nlbin,dusttype))
+else:
+    DLdc = np.load(Pathload+"/power_spectra/DLcross_nside%s_fsky%s_scale%s_Nlbin%s_d%ss%sc.npy"%(nside,fsky,scale,Nlbin,dusttype,synctype))
+
+# Initialize binning scheme with Nlbin ells per bandpower
+
+b = nmt.NmtBin.from_lmax_linear(lmax=lmax,nlb=Nlbin,is_Dell=True)
+l = b.get_effective_ells()
+Nell = 12#len(l)
+
+#instrument informations:
+
+instr_name = 'litebird_full'
+instr =  np.load("./lib/instr_dict/%s.npy"%instr_name,allow_pickle=True).item()
+freq = instr['frequencies']
+freq = freq
+nf = len(freq)
+Ncross = int(nf*(nf+1)/2)
+
+#compute cross-frequencies 
+
+nucross = []
+for i in range(0,nf):
+    for j in range(i,nf):
+        nucross.append(np.sqrt(freq[i]*freq[j]))
+nucross = np.array(nucross)
+
+#compute Cholesky matrix:
+
+if np.shape(np.argwhere(DLdc == 0))[0] == 0:
+    Ncov = len(DLdc)
+else:
+    Ncov = np.argwhere(DLdc == 0)[0,0]-1
+
+if all_ell:
+    if cov_type == 'sim':
+        Linvdc = cvl.getLinv_all_ell(DLdc[:Ncov,:,:Nell],printdiag=True)
+    else:
+        cov = np.load(Pathload+"/covariances/cov_%s_nside%s_fsky%s_scale%s_Nlbin%s_d%ss%sc_all_ell.npy"%(cov_type,nside,fsky,scale,Nlbin,dusttype_cov,synctype_cov))
+        Linvdc = cvl.inverse_covmat(cov, Ncross, neglect_corbins=False, return_cholesky=True, return_new=False)
+
+else:
+    if cov_type == 'sim':
+        Linvdc = cvl.getLinvdiag(DLdc[:Ncov,:,:Nell],printdiag=True)
+    else:
+        cov = np.load(Pathload+"/covariances/cov_%s_nside%s_fsky%s_scale%s_Nlbin%s_d%ss%sc.npy"%(cov_type,nside,fsky,scale,Nlbin,dusttype_cov,synctype_cov))
+        Linvdc = cvl.inverse_covmat(cov, Ncross, neglect_corbins=True, return_cholesky=True, return_new=False)
+
+#N = len(DLdc[:,0,0]) #in order to have a quicker run, replace by e.g. 50 or 100 here for testing.
+DLdc = DLdc[:N,:,:Nell]
+
+#first guesses:
+
+betabar, tempbar, betasbar = 1.5, 20, -3
+
+p0 = [100, betabar, tempbar, 10, betasbar,0, 0] #first guess for mbb A, beta, T, A_s, beta_s, A_sd and r
+results_ds_o0 = an.fit_mom('ds_o0',nucross,DLdc,Linvdc,p0,quiet=True,nside=nside, Nlbin=Nlbin, fix=0, all_ell=True,kwsave='d%ss%s_%s'%(dusttype,synctype,fsky)+kw,plotres=plotres)
+
+#update with order 0's best fit:
+
+betabar = np.mean(results_ds_o0['beta_d'])
+tempbar = np.mean(results_ds_o0['T_d'])
+betasbar = np.mean(results_ds_o0['beta_s'])
+
+# fit order 1 in beta and T, get results, save and plot
+
+p0 = [100, betabar, tempbar, 10, betasbar,1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0]
+results_ds_o1bt = an.fit_mom('ds_o1bt',nucross,DLdc,Linvdc,p0,quiet=True,nside=nside, Nlbin=Nlbin, fix=1,all_ell=False,adaptative=adaptative,kwsave='d%ss%s_%s'%(dusttype,synctype,fsky)+kw,plotres=False)
+
+try:
+    mom_an = np.load('./analytical_mom/analytical_mom_nside%s_fsky%s_scale10_Nlbin10_d%ss%s_%s%s%s.npy' % (nside, fsky, dusttype, synctype, betabar, tempbar, betasbar), allow_pickle=True).item()
+except:
+    mom_an = anmomlib.getmom(dusttype, synctype, betabar, tempbar, betasbar, mask, Nlbin=Nlbin, nside=nside)
+    np.save('./analytical_mom/analytical_mom_nside%s_fsky%s_scale10_Nlbin10_d%ss%s_%s%s%s.npy' % (nside, fsky, dusttype, synctype, betabar, tempbar, betasbar), mom_an)
+
+reslist = [results_ds_o1bt]
+leglist = ['d%ss%s_fsky%s_full'%(dusttype,synctype,fsky)]
+collist = ['darkred']
+
+plotrespdf(l,reslist,leglist,collist,mom_an,plot_contours=plot_contours,betabar=betabar,tempbar=tempbar,betasbar=betasbar)
