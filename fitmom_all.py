125623b704be3404f4033a56fe612446c564b035
LeoVacher <57897650+LeoVacher@users.noreply.github.com>
2025-05-11 14:35:49 +0200

    Update fitmom_all.py

--- fitmom_all.py
+++ fitmom_all.py
@@ -136,4 +136,4 @@ leglist = ['d%ss%s_fsky%s_full'%(dusttype,synctype,fsky)]
 collist = ['darkred']
 
 
-plotrespdf(l,reslist,leglist,collist,mom_an,plot_contours=plot_contours,betabar=betadbar,tempbar=tempbar,betasbar=betasbar)
+plotrespdf(l,reslist,leglist,collist,mom_an,plot_contours=True,betabar=betadbar,tempbar=tempbar,betasbar=betasbar)
