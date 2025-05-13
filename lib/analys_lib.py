b48c2ffe784f92d9f0ff02a929119f84aff38cd4
LeoVacher <57897650+LeoVacher@users.noreply.github.com>
2025-05-13 16:07:19 +0200

    Update analys_lib.py

--- lib/analys_lib.py
+++ lib/analys_lib.py
@@ -128,6 +128,7 @@ def fit_mom(kw,nucross,DL,Linv,p0,quiet=True,parallel=False,nside = 64, Nlbin = 
                 elif kw == 'ds_o1bts':
                     parinfopl[L,18] = {'value': 0, 'fixed': fixr}  # r        
 
+        print(parinfopl)
         if adaptative==True:
             res0=np.load('./best_fits/results_%s_%s.npy'%(kwsave,kwf),allow_pickle=True).item()
             keys= res0.keys()
