5e00f536da73497e8201d682fd12756ad8e31f50
LeoVacher <57897650+LeoVacher@users.noreply.github.com>
2025-10-14 21:50:24 +0200

    remove useless imports and clean routine for mask creation

--- masks/create-masks.py
+++ masks/create-masks.py
@@ -4,24 +4,11 @@ sys.path.append("./lib")
 import numpy as np
 import healpy as hp
 import pymaster as nmt 
-import pysm3
-import time
-from mpfit import mpfit
-import mpfitlib as mpl
-import scipy
-#from Nearest_Positive_Definite import *
-from matplotlib.backends.backend_pdf import PdfPages
-import matplotlib.patheffects as path_effects
-import scipy.stats as st
-import basicfunc as func
-import analys_lib as an
-import simu_lib as sim
-import pysm3.units as u
 import urllib.request
 
-fsky=0.7
+fsky=0.99
 scale=10
-nside=32
+nside=64
 
 download = True
 if download:
