8d51f05b4c077f0bc3c3e84abfe1d40e2806c2de
LeoVacher <57897650+LeoVacher@users.noreply.github.com>
2025-10-14 21:53:27 +0200

    proposed something even cleaner for the mask generation routine

--- masks/create-masks.py
+++ masks/create-masks.py
@@ -10,16 +10,16 @@ fsky=0.99
 scale=10
 nside=64
 
-download = True
-if download:
+fskylist=np.array([0.2,0.4,0.6,0.7,0.8,0.9,0.97,0.99])
+field= list(np.where(fskylist==fsky)[0])
+
+try :
+    maskgal =hp.read_map("./masks/HFI_Mask_GalPlane-apo0_2048_R2.00.fits", field=field)
+except:
     url = "http://pla.esac.esa.int/pla/aio/product-action?MAP.MAP_ID=HFI_Mask_GalPlane-apo0_2048_R2.00.fits"
     output_file = "./masks/HFI_Mask_GalPlane-apo0_2048_R2.00.fits"
     urllib.request.urlretrieve(url, output_file)
 
-fskylist=np.array([0.2,0.4,0.6,0.7,0.8,0.9,0.97,0.99])
-field= list(np.where(fskylist==fsky)[0])
-
-maskgal =hp.read_map("./masks/HFI_Mask_GalPlane-apo0_2048_R2.00.fits", field=field)
 maskgal = hp.ud_grade(maskgal*1.,nside_out=nside)
 maskgal[maskgal<0.5] = 0
 maskgal[maskgal>=0.5] = 1
